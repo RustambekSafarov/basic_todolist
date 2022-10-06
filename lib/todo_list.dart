ed8899f8fdf1452b1390a0001a350ca67c61f00d
ZARIFMAC <uzbzarif@gmail.com>
2022-10-06 14:51:38 +0500

    todolist

---
+++ lib/todo_list.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/widgets.dart';
+
+class TodoList extends StatefulWidget {
+  const TodoList({Key? key}) : super(key: key);
+
+  @override
+  State<TodoList> createState() => _TodoListState();
+}
+
+class _TodoListState extends State<TodoList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
